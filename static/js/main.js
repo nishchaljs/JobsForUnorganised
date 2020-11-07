50242dc2d0ceb84d8c1e479265482f4fba8a6705
sanjanareddy226 <54491134+sanjanareddy226@users.noreply.github.com>
2020-11-07 22:35:40 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+ $(function(){
+  var tickerLength = $('.container ul li').length;
+  var tickerHeight = $('.container ul li').outerHeight();
+  $('.container ul li:last-child').prependTo('.container ul');
+  $('.container ul').css('marginTop',-tickerHeight);
+  function moveTop(){
+    $('.container ul').animate({
+      top : -tickerHeight
+    },600, function(){
+     $('.container ul li:first-child').appendTo('.container ul');
+      $('.container ul').css('top','');
+    });
+   }
+  setInterval( function(){
+    moveTop();
+  }, 3000);
+  });
